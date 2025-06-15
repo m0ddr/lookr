13582cdbb8f5c5b0b91a4a06c8fe54edc9bcd0a3
m0ddr <38412336+m0ddr@users.noreply.github.com>
2025-06-15 02:13:40 +0100

    further optimizations to reduce website footprint

--- content/_index.md
+++ content/_index.md
@@ -4,6 +4,6 @@ paginate_by = 3
 insert_anchor_links = "right"
 +++
 
-<span class='dark:text-blue-400 text-blue-600'>**Hey there!</span>**
+<span class='text-primary-light dark:text-primary-dark'>**Hey there!</span>**
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse turpis mi, ullamcorper quis dui vitae, facilisis pretium justo. Cras ac vestibulum nisi. Donec blandit nec arcu vel ultrices. Etiam massa est, suscipit eget lobortis sit amet, vulputate a diam. Proin venenatis vulputate sagittis. Donec tempor lorem libero, sed auctor felis sagittis vel. In ultrices, nisi sit amet sagittis porta, turpis quam accumsan ligula, dignissim tincidunt lectus orci eget quam. Integer suscipit ut libero sed posuere. 
--- static/js/toggle-theme.js
+++ static/js/toggle-theme.js
@@ -1,58 +1,25 @@
-var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+const darkIcon = document.getElementById('theme-toggle-dark-icon');
+const lightIcon = document.getElementById('theme-toggle-light-icon');
+const toggleBtn = document.getElementById('theme-toggle');
 
-// Change the icons inside the button based on previous settings
-if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    themeToggleLightIcon.classList.remove('hidden');
+// Set initial icon based on theme
+if (localStorage.getItem('color-theme') === 'dark' || (!localStorage.getItem('color-theme') && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    lightIcon.classList.remove('hidden');
 } else {
-    themeToggleDarkIcon.classList.remove('hidden');
+    darkIcon.classList.remove('hidden');
 }
 
-var themeToggleBtn = document.getElementById('theme-toggle');
+toggleBtn.addEventListener('click', () => {
+    // Toggle icons
+    darkIcon.classList.toggle('hidden');
+    lightIcon.classList.toggle('hidden');
 
-function setCommentTheme(theme) {
-    const iframe = document.querySelector('iframe.giscus-frame');
-    if (!iframe) return;
-    iframe.contentWindow.postMessage(
-        { giscus: {
-                setConfig: {
-                    theme: theme
-                },
-            },
-        },
-        'https://giscus.app'
-    );
-}
-
-themeToggleBtn.addEventListener('click', function() {
-
-    // toggle icons inside button
-    themeToggleDarkIcon.classList.toggle('hidden');
-    themeToggleLightIcon.classList.toggle('hidden');
-
-    // if set via local storage previously
-    if (localStorage.getItem('color-theme')) {
-        if (localStorage.getItem('color-theme') === 'light') {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-            setCommentTheme('dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-            setCommentTheme('light');
-        }
-
-        // if NOT set via local storage previously
+    // Toggle theme
+    if (localStorage.getItem('color-theme') === 'light' || (!localStorage.getItem('color-theme') && !document.documentElement.classList.contains('dark'))) {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('color-theme', 'dark');
     } else {
-        if (document.documentElement.classList.contains('dark')) {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-            setCommentTheme('light');
-        } else {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-            setCommentTheme('dark');
-        }
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('color-theme', 'light');
     }
-
 });
